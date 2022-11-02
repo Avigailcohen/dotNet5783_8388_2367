0874e63d52e7761eaf285ffa8ab31fc74f83523b
Avigailcohen <avigailcohen17@gmail.com>
2022-11-02 12:32:25 +0200

    Intial code+Welcome function

---
+++ Stage0/Program8388.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Stage0
+{
+    partial class program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8388();
+            Welcome2367();
+            Console.ReadKey();
+        }
+        static partial void Welcome2367();
+
+        private static void Welcome8388()
+        {
+            Console.WriteLine("Enter your name");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", username);
+        }
+    }
+   
+}
