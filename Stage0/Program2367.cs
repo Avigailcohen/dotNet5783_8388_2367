f0588104052adab7d68d8825806121232ae113da
ItIsraeli <Iti34661@gmail.com>
2022-11-02 12:50:15 +0200

    1249

---
+++ Stage0/Program2367.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Stage0
+{
+	partial class Program
+	{
+		static partial void Welcome2367()
+		{
+			Console.WriteLine("I am also here!")
+		}
+	}
+}
